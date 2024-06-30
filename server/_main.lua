4185b9aa361f51e446211fbe0cae8b86130ee8a3
KamuiKody <62488881+KamuiKody@users.noreply.github.com>
2024-06-30 09:36:40 -0400

    Update _main.lua
    remove framework

--- server/_main.lua
+++ server/_main.lua
@@ -1,5 +1,3 @@
-local QBCore = exports['qb-core']:GetCoreObject()
-
 local function validateDB()
     MySQL.ready(function()
         MySQL.Sync.execute([=[
@@ -14,38 +12,3 @@ local function validateDB()
     end)
 end
 
-local function tradeItems(src, tradeTable, rewardsTable, takeItems)
-    local missingItems = {}
-    local traded = true
-    local itemAction = false
-    local Player = QBCore.Functions.GetPlayer(src)
-    for itemName, amount in pairs(tradeTable) do
-        if takeItems then
-            itemAction = Player.Functions.RemoveItem(itemName, amount) 
-        else
-            itemAction = amount > Player.Functions.GetItemByName(itemName).amount
-        end
-        if not itemAction then
-            traded = false
-            missingItems[itemName] = amount
-        end
-    end
-    if not traded then return false, missingItems end
-    if not takeItems then
-        for itemName, amount in pairs(rewardsTable) do
-            itemAction = Player.Functions.AddItem(itemName, amount)
-            if not itemAction then traded = false end
-        end
-    end
-    return traded, {}
-end
-
-RegisterNetEvent('QBCore:Server:UpdateObject', function()
-	if source ~= '' then return false end
-	QBCore = exports['qb-core']:GetCoreObject()
-end)
-
-AddEventHandler('onResourceStart', function(resourceName)
-    if GetCurrentResourceName() ~= resourceName then return end
-    if Config.Items then exports['qb-core']:AddItems(Config.Items) end
-end)
